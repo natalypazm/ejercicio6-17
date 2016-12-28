dc6060cc0524de95db2dfa62c8464901c6d4f975
natalypazm <1000104644.rr@gmail.com>
2016-12-28 14:37:50 -0500

    add css

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function calcular()
 	var b =parseInt(document.getElementById("b").value);
 	var salida = document.getElementById("salida");
 	if ((a == 10 || b == 10) ||( a+b ==10 )){
+	
 		salida.innerHTML = 1;
 	}
 	else{
